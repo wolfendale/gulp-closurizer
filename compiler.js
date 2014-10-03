8b9b8cb02f82aad5e4b3860f3eacc7c67f84e807
Michael Wolfendale <michael.wolfendale@gmail.com>
2014-10-03 07:58:43 +0100

    More comments and refactoring
    Code is most likely broken, will fix later. Refactoring the util file
    into the compiler. Making code a little neater and adding comments.

--- compiler.js
+++ compiler.js
@@ -24,6 +24,93 @@ module.exports 	= function(opts) {
 		this.args 		= ['-jar', serializer.serialize(j_opts), this.path];
 	};
 
+	Compiler.prototype._setup 	= function(files) {
+
+		var mkTmpDir, mkTmpFiles, mkFlagFile, deferred;
+
+		deferred = q.defer();
+
+		mkTmpDir()
+		.then(mkTmpFiles(files))
+		.then(mkFlagFile)
+		.then(function(flagfile) {
+
+			deferred.resolve(flagfile);
+		})
+		.fail(function(e) {
+
+			deferred.reject(e);
+		});
+
+		return deferred.promise;
+
+		mkTmpDir 	= function() {
+
+			var deferred = q.defer();
+
+			q.ninvoke(tmp, 'dir', {dir: '.', unsafeCleanup: !opts.debug})
+			.then(function(tmpdir) {
+
+				deferred.resolve(tmpdir[0]);
+			})
+			.fail(function(e) {
+
+				deferred.reject(e);
+			});
+
+			return deferred;
+		};
+
+		mkTmpFiles 	= function(files) {
+
+			/*
+			 *	Some manual currying here.
+			 */
+			return function(tmpdir) {
+
+				var deferred	= q.defer();
+
+				q.all(_.map(files, function(file) {
+
+					var deferred	= q.defer(),
+						filePath	= path.resolve(tmpdir + '/' + file.relative);
+
+					q.ninvoke(fs, 'writeFile',
+						filePath,
+						file.contents)
+					.then(function() {
+
+						deferred.resolve(filePath);
+					})
+					.fail(deferred.reject);
+
+					return deferred.promise;
+				}))
+				.then(function(paths) {
+
+					deferred.resolve([tmpdir, paths]);
+				})
+				.fail(function(deferred.reject);
+
+				return deferred.promise;
+			};
+		};
+
+		mkFlagFile 	= function(args) {
+
+			var deferred 	= q.defer();
+
+			return deferred.promise;
+		}
+	};
+
+	/*
+	 *	This is the main compilation method, it is responsible for creating the
+	 *	temporary files required by the compiler and then spawning the java
+	 *	process which will run the actual compilation. Currently we return data
+	 *	from the stdout, however we may need to utilise another temporary file
+	 * 	instead.
+	 */
 	Compiler.prototype.compile = function(files) {
 
 		var deferred 	= q.defer(),
--- serializer.js
+++ serializer.js
@@ -1,5 +1,13 @@
-var _ 	= require('lodash');
+var _	 	= require('lodash'),
+	gutil	= require('gulp-util');
 
+/*
+ *	Class for dealing serializing objects into command line arguments.
+ *	Other implementations prefix flags automatically and other magic,
+ * 	however I don't see much point, this will pass compiler/java flags
+ *	straight through so make sure they're defined in an object with
+ * 	the keys delimited by ''
+ */
 var Serializer = function(opts) {
 
 	opts 		= opts || {};
@@ -11,12 +19,15 @@ var Serializer = function(opts) {
 		valDelim	: ',',
 		kPre		: '',
 		kPost		: '',
-		vPre		: '',
-		vPost		: '',
+		vPre		: '"',
+		vPost		: '"',
 		repeatKey	: true
 	});
 };
 
+/*
+ *	Formats a flag 'key' with an optional prefix/suffix
+ */
 Serializer.prototype.formatKey 		= function(key) {
 
 	return [this.opts.kPre,
@@ -24,6 +35,9 @@ Serializer.prototype.formatKey 		= function(key) {
 			this.opts.kPost].join('');
 };
 
+/*
+ *	Formats a flag value with an optional prefix/suffix
+ */
 Serializer.prototype.formatValue	= function(val) {
 
 	return [this.opts.vPre,
@@ -31,6 +45,17 @@ Serializer.prototype.formatValue	= function(val) {
 			this.opts.vPost].join('');
 };
 
+/*
+ *	This is the main serialization method, it reduces an object
+ * 	to a string of flags, if `repeatKey` is true, array values
+ *	will have the key repeated for each occurance of the value.
+ *
+ *	e.g. flag="val1" flag="val2" 
+ *
+ *	Default flag format is key="val", if you wanted to enforce
+ *	`--` at the start of a flag, you could set the `kPre` property
+ *	to `--`.
+ */
 Serializer.prototype.serializeObject 	= function(args) {
 
 	var self = this, s;
@@ -70,10 +95,14 @@ Serializer.prototype.serializeObject 	= function(args) {
 	return s.substr(0, s.length - self.opts.flagDelim.length);
 }
 
+/**
+ *	Entry point for serialization, does some type checking
+ *	and then defers to the relevant function.
+ */
 Serializer.prototype.serialize 			= function(arg) {
 
 	if (_.isUndefined(arg))
-		throw 'args must be defined';
+		throw new Error('`arg` must be defined');
 
 	if (_.isString(arg)) {
 
